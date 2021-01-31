667bef8931167e5270156efda08eee793a10b20e
mgreminger <michael.greminger@gmail.com>
2021-01-30 19:47:41 -0600

    Added capability to import excel file

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
   import EditableTable from "./EditableTable.svelte";
   import Tabs from "./Tabs.svelte";
   import Plot from "./Plot.svelte";
-  import { data, parameters, parameterTypes, parameterOptions,
+  import { data, dataText, parameters, parameterTypes, parameterOptions,
            numParetoPoints, fullyDefined, nonTargetOutputs, xAxisOutput,
            yAxisOutput } from './stores.js';
 
@@ -14,6 +14,7 @@
 
   let selectedTab = 0;
   let plotData = null;
+  let xlsxLoaded = false;
 
   function getParetoData(){
     pyodideWorker.postMessage([$data, $parameters, $parameterTypes, $parameterOptions, $numParetoPoints]);
@@ -27,6 +28,33 @@
       plotData = e.data;
     }
   }
+
+  function updateXlsxLoaded(){
+    xlsxLoaded = true;
+  }
+
+  function handleFile(e) {
+    if (xlsxLoaded) {
+      let files = e.target.files, f = files[0];
+      let reader = new FileReader();
+      reader.onload = function(e) {
+        let data = new Uint8Array(e.target.result);
+        let workbook = XLSX.read(data, {type: 'array'});
+
+        let first_sheet_name = workbook.SheetNames[0];
+
+        let worksheet = workbook.Sheets[first_sheet_name];
+
+        let new_data = XLSX.utils.sheet_to_json(worksheet, {header: 1});
+
+        $parameters = new_data[0];
+        $dataText = new_data.slice(1);
+      } 
+      reader.readAsArrayBuffer(f);
+    } else {
+          alert("XLSX Library not loaded yet, please try again.");
+    }
+  };
 
   $: if(!$fullyDefined) {
     plotData = null;
@@ -45,9 +73,14 @@
   }
 </style>
 
+<svelte:head>
+  <script src="./xlsx/xlsx.full.min.js" on:load={updateXlsxLoaded}></script>
+</svelte:head>
+
 <Tabs tabs={['Input Data', 'Pareto Plot', 'Pareto Data']}
       bind:selectedTab>
   <div class="{selectedTab === 0 ? '' : 'hidden'}">
+    <input type="file" on:change={handleFile}/>
     <EditableTable />
   </div>
   <div class="{selectedTab === 1 ? '' : 'hidden'}">
